787f1768c49aaa6fa06b95aa6191aa3087ff25d8
DanielAnyien <ppprocash@gmail.com>
2021-09-11 15:36:05 +0700

    Incremental Game

---
+++ Assets/Script/TapArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource coinSound;
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+        coinSound.Play();
+    }
+
+}
